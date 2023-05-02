a857f3bf8479f3865f42838c7ff2fd5c829eb003
sas-miles <132398659+sas-miles@users.noreply.github.com>
2023-05-02 15:58:17 -0400

    test

--- theater-playground.js
+++ theater-playground.js
@@ -1,8 +1,8 @@
-import "https://cdn.jsdelivr.net/npm/@theatre/browser-bundles@0.5.0-insiders.88df1ef/dist/core-and-studio.js";
+
 
 // We can now access Theatre.core and Theatre.studio from here
 const { core, studio } = Theatre;
 
 studio.initialize(); // Start the Theatre.js UI
 
-console.log("Theatre.js code is running!");
+console.log("Theatre.js code is running!!");
